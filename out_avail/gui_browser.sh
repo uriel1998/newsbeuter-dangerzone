d270a8f19ea74e652645d3356950e6cdfc71a7ff
Steven Saus <steven@stevesaus.com>
2020-10-10 15:16:32 -0400

    Added a duh gui browser option

---
+++ out_avail/gui_browser.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function gui_browser_send {
+    
+    /home/steven/apps/firefox/firefox --new-tab "$link" 
+
+}
+##############################################################################
+# Are we sourced?
+# From http://stackoverflow.com/questions/2683279/ddg#34642589
+##############################################################################
+
+# Try to execute a `return` statement,
+# but do it in a sub-shell and catch the results.
+# If this script isn't sourced, that will raise an error.
+$(return >/dev/null 2>&1)
+
+# What exit code did that give?
+if [ "$?" -eq "0" ];then
+    echo "[info] Function ready to go."
+    OUTPUT=0
+else
+    OUTPUT=1
+    if [ "$#" = 0 ];then
+        echo -e "Please call this as a function or with \nthe url as the first argument and optional \ndescription as the second."
+    else
+        link="${1}"
+        if [ ! -z "$2" ];then
+            title="$2"
+        fi
+        gui_browser_send
+    fi
+fi
