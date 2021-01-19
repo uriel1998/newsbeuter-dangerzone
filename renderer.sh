923faef6958f029124b6d53b448bc6dac39b4726
Steven Saus <steven@stevesaus.com>
2021-01-19 14:47:10 -0500

    Update to fix stdin better

--- renderer.sh
+++ renderer.sh
@@ -12,9 +12,8 @@ TempDir=$(mktemp -d)
 ArticleFile="$TempDir/Article.html"
 OutFile="$TempDir/OutFile.html"
 
-while IFS= read -r line; do
-  printf '%s\n' "$line" >> $ArticleFile
-done
+[ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
+cat $input > "$ArticleFile"
 
 cat $ArticleFile | sed -e 's/<img[^>]*>//g' | sed -e 's/<div[^>]*>//g' | hxclean | hxnormalize -e -L -s 2>/dev/null | tidy -quiet -omit -clean 2>/dev/null | hxunent | iconv -t utf-8//TRANSLIT - | sed -e 's/\(<em>\|<i>\|<\/em>\|<\/i>\)/&🞵/g' | sed -e 's/\(<strong>\|<b>\|<\/strong>\|<\/b>\)/&🞶/g' |lynx -dump -stdin -display_charset UTF-8 -width 140 | sed -e 's/\*/•/g' | sed -e 's/Θ/🞵/g' | sed -e 's/Φ/🞯/g' | sed 's/^/
 \t/'
