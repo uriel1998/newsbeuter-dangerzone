8e315c4fa9a7e158d024813ef78547c998d90276
Steven Saus <steven@stevesaus.com>
2024-10-05 08:10:30 -0400

    Hoo boy, got the renderer fancified, using pup and all sorts of things to make it pretty for both mutt and newsboat

--- renderer_links.sh
+++ renderer_links.sh
@@ -2,23 +2,24 @@
 
 ##############################################################################
 #
-#  My renderer for newsboat/newsbeuter
+#  My renderer for newsboat/newsbeuter/mutt
 #  (c) Steven Saus 2024
 #  Licensed under the MIT license
 #
 ##############################################################################
 
-
-## may consider having it output as gfm and then having rich do it with -m --hyperlinks
-                                                                        
-
 if [ $# -eq 0 ]; then                                                 
     # no arguments passed, use stdin
     input=$(cat)
-        echo "${input}" | sed -e 's/<img[^>]*>//g' | sed -e 's/<div[^>]*>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | iconv -t utf-8//TRANSLIT - | elinks -dump -dump-charset UTF-8 -dump-width 140 | grep -v "READ MORE:" 
-#    echo "<html><body>" > /home/steven/reference_article.txt
-#    echo $input | sed -e 's/<img[^>]*>//g' | sed -e 's/<div[^>]*>//g' | sed -e 's/<!-- -->//g' |  hxunent | iconv -t utf-8//TRANSLIT - >> /home/steven/reference_article.txt
-    #echo "</body></html>" >> /home/steven/reference_article.txt
+    antimatch=""
+    antimatch=$(echo "${input}" | pup 'div[style*="display: none;"],div[style*="display:none;"], div[style*="visibility: hidden;"], div[style*="overflow: hidden;"]')
+    if [ "$antimatch" != "" ];then
+        echo " "
+        echo "${input}"  | pup | grep -vF "${antimatch}" | sed -e 's/<div[^>]*>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | elinks -dump -dump-width 140 | grep -v "READ MORE:"             
+    else
+        echo " "
+        echo "${input}"  | pup | sed -e 's/<div[^>]*>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | elinks -dump -dump-width 140 | grep -v "READ MORE:" 
+    fi
 else
     # it's a URL, pass to elinks directly
     if [ $(echo "${1}" | grep -c http) -gt 0 ];then
@@ -28,7 +29,17 @@ else
     # it's a file, parse it this way
     if [ -f "${1}" ];then
         input=$(cat "${1}")
-        echo $input | sed -e 's/<img[^>]*>//g' | sed -e 's/<div[^>]*>//g' | hxclean | hxnormalize -e -L -s 2>/dev/null | tidy -quiet -omit -clean 2>/dev/null | hxunent | iconv -t utf-8//TRANSLIT - | elinks -dump -dump-charset UTF-8 -dump-width 140 
+        # this is to deal with stupid hidden divs in HTML emails
+        # should only invoke when it's there; otherwise it craps out the whole thing.
+        antimatch=""
+        antimatch=$(echo "${input}" | pup 'div[style*="display: none;"],div[style*="display:none;"], div[style*="visibility: hidden;"], div[style*="overflow: hidden;"]')
+        if [ "$antimatch" != "" ];then
+            echo " "
+            echo "${input}"  | pup | grep -vF "${antimatch}" | sed -e 's/<div[^>]*>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | elinks -dump -dump-width 140 | grep -v "READ MORE:"             
+        else
+            echo " "
+            echo "${input}"  | pup | sed -e 's/<div[^>]*>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | elinks -dump -dump-width 140 | grep -v "READ MORE:" 
+        fi
     fi
 
 fi                                                                        
