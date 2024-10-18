dc9b2a4e767269961bf6d119db64bd503b257f3c
Steven Saus <steven@stevesaus.com>
2024-10-18 18:39:12 -0400

    optimized, has rich, made emphasis (em, strong) a little better formatted

--- renderer.sh
+++ renderer.sh
@@ -18,42 +18,37 @@
 # (also, regular markdown characters don't work, so I don't forget AGAIN and try AGAIN)
 # adding a br after each table row so it doesn't become collapsed and we have SOME whitespace
 # considering -nonumbers to clean up the body text, but...
+# using rich to draw a box around the text and format it a little more nicely
+
+PROCESSED=""
 
 if [ $# -eq 0 ]; then                                                 
     # no arguments passed, use stdin
     input=$(cat)
+else     
+    if [ $(echo "${1}" | grep -c http) -gt 0 ];then
+        PROCESSED=$(elinks "${1}" -dump -dump-charset UTF-8 -dump-width 130)
+    else
+        # it's a file, parse it this way
+        if [ -f "${1}" ];then
+            input=$(cat "${1}")
+        else
+            echo "I don't know what to do with this."
+        fi
+    fi
+fi
+if [ -z "$PROCESSED" ];then    
     antimatch=""
     antimatch=$(echo "${input}" | pup 'div[style*="display: none;"],div[style*="display:none;"], div[style*="visibility: hidden;"], div[style*="overflow: hidden;"]')
         if [ "$antimatch" != "" ];then
             echo " "
-            echo "${input}"  | pup | grep -vF "${antimatch}" | sed -e 's/<div[^>]*>//g' | sed 's/<img[^>]\+>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | sed -e 's/<\/tr>/<\/tr><br \/>/g'| hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | lynx -dump -stdin -assume_charset=UTF-8 -force_empty_hrefless_a -hiddenlinks=ignore -html5_charsets -dont_wrap_pre -nolist -width=140 -collapse_br_tags | grep -v "READ MORE:" | rich -m -a rounded -d 2,2,2,2 -y --soft --print -W 140 -w 138 -
+            PROCESSED=$(echo "${input}"  | pup | grep -vF "${antimatch}" | sed -e 's/<div[^>]*>//g' | sed 's/<img[^>]\+>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/§⬞/g' | sed -e 's/<\/em>/⬞§/g' | sed -e 's/<strong[^>]*>/§⬞/g' | sed -e 's/<\/strong>/⬞§/g' | sed -e 's/<\/tr>/<\/tr><br \/>/g'| hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | lynx -dump -stdin -nolist -assume_charset=UTF-8 -force_empty_hrefless_a -hiddenlinks=ignore -html5_charsets -dont_wrap_pre -width=130 -collapse_br_tags | grep -v "READ MORE:" )
         else
             echo " "
-            echo "${input}"  | pup | sed -e 's/<div[^>]*>//g' | sed 's/<img[^>]\+>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | sed -e 's/<\/tr>/<\/tr><br \/>/g'| hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | lynx -dump -stdin -assume_charset=UTF-8 -force_empty_hrefless_a -hiddenlinks=ignore -html5_charsets -dont_wrap_pre -nolist -width=140 -collapse_br_tags | grep -v "READ MORE:" | rich -m -a rounded -d 2,2,2,2 -y --soft --print -W 140 -w 138 -
+            PROCESSED=$(echo "${input}"  | pup | sed -e 's/<div[^>]*>//g' | sed 's/<img[^>]\+>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/§⬞/g' | sed -e 's/<\/em>/⬞§/g' | sed -e 's/<strong[^>]*>/§⬞/g' | sed -e 's/<\/strong>/⬞§/g' | sed -e 's/<\/tr>/<\/tr><br \/>/g'| hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | lynx -dump -stdin -nolist  -assume_charset=UTF-8 -force_empty_hrefless_a -hiddenlinks=ignore -html5_charsets -dont_wrap_pre -width=130 -collapse_br_tags | grep -v "READ MORE:" )
         fi
-    else
-    # it's a URL, pass to elinks directly
-    if [ $(echo "${1}" | grep -c http) -gt 0 ];then
-        elinks "${1}" -dump -dump-charset UTF-8 -dump-width 140
-        exit
-    fi
-    # it's a file, parse it this way
-    if [ -f "${1}" ];then
-        input=$(cat "${1}")
-        # this is to deal with stupid hidden divs in HTML emails
-        # should only invoke when it's there; otherwise it craps out the whole thing.
-        echo "${input}" > /home/steven/tmp/shit.txt
-        antimatch=""
-        antimatch=$(echo "${input}" | pup 'div[style*="display: none;"],div[style*="display:none;"],div[style*="visibility: hidden;"],div[style*="overflow: hidden;"]')
-        if [ "$antimatch" != "" ];then
-            echo " "
-            echo "${input}"  | pup | grep -vF "${antimatch}" | sed -e 's/<div[^>]*>//g' | sed 's/<img[^>]\+>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | sed -e 's/<\/tr>/<\/tr><br \/>/g' | hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | lynx -dump -stdin -assume_charset=UTF-8 -force_empty_hrefless_a -underscore -hiddenlinks=ignore -html5_charsets -dont_wrap_pre -nolist -width=140 | grep -v "READ MORE:"             
-        else
-            echo " "
-            echo "${input}"  | pup | sed -e 's/<div[^>]*>//g' | sed 's/<img[^>]\+>//g' | sed -e 's/<!-- -->//g'| sed -e 's/<em[^>]*>/⬞/g' | sed -e 's/<\/em>/⬞/g' | sed -e 's/<strong[^>]*>/⬞/g' | sed -e 's/<\/strong>/⬞/g' | sed -e 's/<\/tr>/<\/tr><br \/>/g'| hxclean | hxnormalize -e -L -s 2>/dev/null | hxunent | lynx -dump -stdin -assume_charset=UTF-8 -force_empty_hrefless_a -hiddenlinks=ignore -html5_charsets -dont_wrap_pre -nolist -width=140 -collapse_br_tags | grep -v "READ MORE:" 
-        fi
-    fi
-fi                                                                        
+fi        
 
- 
+# This isn't perfect -- multiline doesn't work at all, and combos of italics and strongs confuse it, but... it's readable?
+printf "%s" "${PROCESSED}" | sed -e 's/ ⬞/⬞/g' -e 's/ ⬞/⬞/g' | sed 's/⬞§ *§⬞//g'  |  sed -e 's/⬞ /⬞/g' -e 's/⬞ /⬞/g' -e 's/§//g' | rich -m -a rounded -d 2,2,2,2 -y --soft --print -W 140 -w 138 -
 
