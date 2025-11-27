32fa422fa536eba062a93fd5eafafaf643aa1c10
Steven Saus <steven@stevesaus.com>
2025-11-27 10:13:18 -0500

    chore(notes.txt): update notes to clarify configuration for mutt profile directory chore(onews): export calling_program variable for better configuration management fix(toot.sh): update configuration file path to use calling_program for improved flexibility in loading settings

--- out_avail/toot.sh
+++ out_avail/toot.sh
@@ -20,15 +20,37 @@ function loud() {
 
 
 function toot_send {
+
+    # check some config things that SHOULD be set, etc.
+    if [ -n "${my_CONFIG_DIR}" ];then
+        # if the variable is set and exported, they've probably set it up properly.
+        ConfigFile="${my_CONFIG_DIR}/newsbeuter_dangerzone.ini"
+    else
+        loud "[WARN] Configuration variable not set, checking default location."
+        # try to find a default quickly.
+        CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/newsbeuter_dangerzone"
+        # here, though, we're doublechecking.
+        if [ -f "${CONFIG_DIR}/newsbeuter_dangerzone.ini" ];then
+            ConfigFile="${CONFIG_DIR}/newsbeuter_dangerzone.ini"
+        else
+            loud "[ERROR] Configuration not found at"
+            loud "[ERROR] ${CONFIG_DIR}/newsbeuter_dangerzone.ini"
+            exit 97
+        fi
+    fi
+
+
+
     tempfile=$(mktemp)
     if [ "$title" == "$link" ];then
         title=""
     fi
 
-    account_using=$(grep 'mastodon =' "${XDG_CONFIG_HOME}/agaetr/agaetr.ini" | sed 's/ //g' | awk -F '=' '{print $2}')
-    binary=$(grep 'toot =' "${XDG_CONFIG_HOME}/agaetr/agaetr.ini" | sed 's/ //g' | awk -F '=' '{print $2}')
+    account_using=$(grep 'mastodon =' "${ConfigFile}" | sed 's/ //g' | awk -F '=' '{print $2}')
+    binary=$(grep 'toot =' "${ConfigFile}" | sed 's/ //g' | awk -F '=' '{print $2}')
+    # and this is where I find out the dude made his own crossposter.  Which, cool, but single point of failure, etc.
 
-
+    # URL length (at least for one) counts for 23.  So let's fix our math and avoid shortening (and then port to agaetr)
     #Yes, I know the URL length doesn't actually count against it.  Just
     #reusing code here.
     bigstring=$(printf "(%s) %s \n\n%s \n\n%s \n%s \n\n%s" "$pubtime" "$title" "$description" "$link" "${description2}" "$hashtags")
@@ -123,6 +145,7 @@ function toot_send {
 # From http://stackoverflow.com/questions/2683279/ddg#34642589
 ##############################################################################
 
+
 # Try to execute a `return` statement,
 # but do it in a sub-shell and catch the results.
 # If this script isn't sourced, that will raise an error.
@@ -141,11 +164,14 @@ else
             LOUD=1
             shift
         else
-            LOUD=0
+            if [ "$LOUD" == "" ];then
+                # so it doesn't clobber exported env
+                LOUD=0
+            fi
         fi
         link="${1}"
         if [ ! -z "$2" ];then
-            title="$2"
+            title="$2" # These should already be cleaned.
         fi
         toot_send
     fi
