fdb3bd3600417d3d59594bb852b7f22a590eb1ce
Steven Saus <steven@stevesaus.com>
2025-12-01 21:22:45 -0500

    feat(bluesky.sh): add configuration checks for my_CONFIG_DIR and default location for newsbeuter_dangerzone.ini to improve robustness refactor(bluesky.sh): streamline output formatting logic to handle long strings and ensure proper trimming of title and descriptions style(bluesky.sh): clean up whitespace and formatting for better readability

--- out_avail/bluesky.sh
+++ out_avail/bluesky.sh
@@ -23,58 +23,85 @@ function loud() {
 
 
 function bluesky_send {
-    
+
+    # check some config things that SHOULD be set, etc.
+    if [ -n "${my_CONFIG_DIR}" ];then
+        # if the variable is set and exported, they've probably set it up properly.
+        ConfigFile="${my_CONFIG_DIR}/newsbeuter_dangerzone.ini"
+    else
+        loud "[WARN] Configuration variable not set, checking default location."
+        # try to find a default quickly.
+        CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/newsbeuter_dangerzone"
+        # here, though, we're doublechecking.
+        if [ -f "${CONFIG_DIR}/newsbeuter_dangerzone.ini" ];then
+            ConfigFile="${CONFIG_DIR}/newsbeuter_dangerzone.ini"
+        else
+            loud "[ERROR] Configuration not found at"
+            loud "[ERROR] ${CONFIG_DIR}/newsbeuter_dangerzone.ini"
+            exit 97
+        fi
+    fi
     tempfile=$(mktemp)
-    
     if [ "$title" == "$link" ];then
         title=""
     fi
-     
-    binary=$(grep 'bluesky =' "${XDG_CONFIG_HOME}/agaetr/agaetr.ini" | sed 's/ //g' | awk -F '=' '{print $2}')
- 
+
+    binary=$(grep 'bluesky =' "${ConfigFile}" | sed 's/ //g' | awk -F '=' '{print $2}')
+
     if [ "$description2" != "" ];then
         description2="Archive: ${description2}"
     fi
-    bigstring=$(printf "(%s) %s \n\n%s \n\n%s \n%s \n\n%s" "$pubtime" "$title" "$description" "$link" "${description2}" "$hashtags")
-    
-    if [ ${#bigstring} -lt 300 ];then 
-        printf "(%s) %s \n\n%s \n\n%s \n%s \n\n%s" "$pubtime" "$title" "$description" "$link" "${description2}" "$hashtags" > "${tempfile}"
-    else
-        outstring=$(printf "(%s) %s \n\n%s \n\n%s \n\n%s" "$pubtime" "$title" "$link" "$description2" "$hashtags")
-        if [ ${#outstring} -lt 300 ]; then
-            printf "(%s) %s \n\n%s \n\n%s \n\n%s" "$pubtime" "$title" "$link" "$description2" "$hashtags" > "${tempfile}"
+    outstring=$(printf "%s  \n\n%s  \n\n%s  \n\n%s  \n\n%s" "${title}" "${description}" "${description2}" "${link}" "${hashtags}")
+
+    if [ ${#outstring} -gt 300 ];then
+
+        # testing length description, which is either from the feed, null (default newsboat/mutt), or *user set* from newsboat/mutt.
+        tlen=$(( ${#title} + 3 )) # accounting for newlines
+        d1len=$(( ${#description} + 3 ))
+        d2len=$(( ${#description2} + 3 ))
+        hashlen=$(( ${#hashtags} + 3 ))
+        urlen=$(( ${#link} + 3 ))
+        total_length=$(( tlen + d1len + d2len + hashlen + urlen ))
+        diff_len=$(( 300 - total_length ))
+        if [ $diff_len -lt 0 ]; then
+            printf "%s \n\n%s \n\n%s \n\n%s \n\n%s" "${title}" "${description}" "${description2}" "${link}" "${hashtags}" > "${tempfile}"
         else
-            outstring=$(printf "(%s) %s \n\n%s \n\n%s" "$pubtime" "$title" "$description2" "$link")
-            if [ ${#outstring} -lt 300 ]; then
-                printf "(%s) %s \n\n%s \n\n%s" "$pubtime" "$title" "$description2" "$link" > "${tempfile}"
+            if [ $hashlen -gt $diff_len ];then
+                printf "%s  \n\n%s  \n\n%s  \n\n%s" "${title}" "${description}" "${description2}" "${link}" > "${tempfile}"
             else
-                outstring=$(printf "%s \n\n%s \n\n%s" "$title" "$description2" "$link")
-                if [ ${#outstring} -lt 300 ]; then
-                    printf "%s \n\n%s \n\n%s" "$title" "$description2" "$link" > "${tempfile}"
+                diff_len=$(( diff_len - hashlen ))
+                if [ $d2len -gt $diff_len ];then
+                    trimto=$(( d2len - diff_len - 4 ))
+                    description2="${description2:0:trimto}... "
+                    printf "%s  \n\n%s  \n\n%s  \n\n%s" "${title}" "${description}" "${description2}" "${link}" > "${tempfile}"
                 else
-                    outstring=$(printf "(%s) %s \n\n%s " "$pubtime" "$title" "$link")
-                    if [ ${#outstring} -lt 300 ]; then
-                        printf "(%s) %s \n\n%s " "$pubtime" "$title" "$link" > "${tempfile}"
+                    diff_len=$(( diff_len - d2len ))
+                    # the difference was more than we could cut out of d2len
+                    if [ $d1len -gt $diff_len ];then
+                        # use d1len and diff_len to figure out how much to trim off d1len, post.
+                        trimto=$(( d1len - diff_len - 4 ))
+                        description="${description:0:trimto}... "
+                        printf "%s  \n\n%s  \n\n%s" "${title}" "${description}" "${link}" > "${tempfile}"
                     else
-                        outstring=$(printf "%s \n\n%s" "$title" "$link")
-                        if [ ${#outstring} -lt 300 ]; then
-                            printf "%s \n\n%s" "$title" "$link" > "${tempfile}"
-                        else
-                            short_title=`echo "$title" | awk '{print substr($0,1,110)}'`
-                            printf "%s \n\n%s" "$short_title" "$link" > "${tempfile}"
-                        fi
+                        diff_len=$(( diff_len - d1len ))
+                        # the difference was more than we could cut out of d1len
+                        trimto=$(( tlen - diff_len - 4 ))
+                        title="${title:0:trimto}... "
+                        printf "%s  \n\n%s" "${title}" "${link}" > "${tempfile}"
+                        # use tlen and diff_len to figure out how much to trim off title, post.
+                        # this test HAS to pass,
                     fi
                 fi
             fi
         fi
+    else
+        # I realize this is a double test.
+        printf "%s \n\n%s \n\n%s \n%s" "${title}" "${description}" "${description2}" "${link}" "${hashtags}" > "${tempfile}"
     fi
-    echo " " >> "${tempfile}"
 
-
-   
     # Get the image, if exists, then send the post
     if [ ! -z "${imgurl}" ];then
-        
+
         if [ -f "${imgurl}" ];then
             filename=$(basename -- "${imgurl}")
             extension="${filename##*.}"
@@ -85,9 +112,9 @@ function bluesky_send {
             curl "${imgurl}" -o "${Outfile}" --max-time 60 --create-dirs -s
         fi
         if [ -f "${Outfile}" ];then
-            loud "[info] Image obtained, resizing."       
+            loud "[info] Image obtained, resizing."
             if [ -f /usr/bin/convert ];then
-                /usr/bin/convert -resize 800x512\! "${Outfile}" "${Outfile}" 
+                /usr/bin/convert -resize 800x512\! "${Outfile}" "${Outfile}"
             fi
             if [ ! -z "${ALT_TEXT}" ];then
                 Limgurl=$(printf " --image \'%s\' --image-alt '%s'" "${Outfile}" "${ALT_TEXT}")
@@ -100,12 +127,12 @@ function bluesky_send {
     else
         Limgurl=""
     fi
-  
+
     postme=$(printf "cat %s | %s post --stdin %s" "${tempfile}" "${binary}"  "${Limgurl}")
     loud "${postme}"
     eval "${postme}"
 
-    
+
     if [ -f "${Outfile}" ];then
         rm "${Outfile}"
     fi
@@ -135,8 +162,11 @@ else
             LOUD=1
             shift
         else
-            LOUD=0
-        fi    
+            if [ "$LOUD" == "" ];then
+                # so it doesn't clobber exported env
+                LOUD=0
+            fi
+        fi
         link="${1}"
         if [ ! -z "$2" ];then
             title="$2"
@@ -144,4 +174,3 @@ else
         bluesky_send
     fi
 fi
-        
